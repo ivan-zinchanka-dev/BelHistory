447d01b352c248403c48939c363dac3318dce63b
Ivan Zinchanka <75093640+ivan-zinchanka-dev@users.noreply.github.com>
2025-05-15 20:13:16 +0300

    feat: more data

--- MongoshScripts/history_docs_data.js
+++ MongoshScripts/history_docs_data.js
@@ -1,7 +1,8 @@
-docsCategory = db.categories.findOne({ "name.be": "Дакументы" })
+const docsCategory = db.categories.findOne({ "name.be": "Дакументы" })
 
-oldEastSlavic = db.languages.findOne({ "name.be": "Старажытнаруская" })
-ruthenian = db.languages.findOne({ "name.be": "Старабеларуская" })  
+const oldEastSlavic = db.languages.findOne({ "name.be": "Старажытнаруская" })
+const ruthenian = db.languages.findOne({ "name.be": "Старабеларуская" })
+const russian = db.languages.findOne({ "name.be": "Руская" })
 
 db.historicalDocs.insertOne({
     "title": {
@@ -19,7 +20,95 @@ db.historicalDocs.insertOne({
     "languageId": ruthenian._id,
     "categoryId": docsCategory._id,
     "subCategoryId": db.categories.findOne({ "name.be": "Права", "parentId": docsCategory._id })._id,
-    "fileId": db.fs.files.findOne({ filename: "D:\\Downloads\\Судзебнік_Казіміра_1468_г.pdf" })._id,
+    "fileId": db.fs.files.findOne({ filename: /Судзебнік_Казіміра_1468_г\.pdf$/i })._id,
+    "tags": [
+        "TestTag0", "TestTag1"
+    ]
+})
+
+db.historicalDocs.insertOne({
+    "title": {
+        "be": "Прывілей Казіміра 1447 г.",
+        "ru": "Привелей Казимира 1447 г."
+    },
+    "creationTime": {
+        "be": "1447 г.",
+        "ru": "1447 г."
+    },
+    "author": {
+        "be": "Казімір IV Ягелончык",
+        "ru": "Казимир IV Ягеллончик"
+    },
+    "languageId": ruthenian._id,
+    "categoryId": docsCategory._id,
+    "subCategoryId": db.categories.findOne({ "name.be": "Права", "parentId": docsCategory._id })._id,
+    "fileId": db.fs.files.findOne({ filename: /Прывілей_Казіміра_1447_г\.pdf$/i })._id,
+    "tags": [
+        "TestTag0", "TestTag1"
+    ]
+})
+
+db.historicalDocs.insertOne({
+    "title": {
+        "be": "Прывілей Аляксандра 1492 г.",
+        "ru": "Привелей Александра 1492 г."
+    },
+    "creationTime": {
+        "be": "1492 г.",
+        "ru": "1492 г."
+    },
+    "author": {
+        "be": "Аляксандр IV Ягелончык",
+        "ru": "Александр IV Ягеллончик"
+    },
+    "languageId": ruthenian._id,
+    "categoryId": docsCategory._id,
+    "subCategoryId": db.categories.findOne({ "name.be": "Права", "parentId": docsCategory._id })._id,
+    "fileId": db.fs.files.findOne({ filename: /Прывілей_Аляксандра_1492_г\.pdf$/i })._id,
+    "tags": [
+        "TestTag0", "TestTag1"
+    ]
+})
+
+db.historicalDocs.insertOne({
+    "title": {
+        "be": "Судзебнік Івана 1497 г. (арыгінал)",
+        "ru": "Судебник Ивана 1497 г. (оригинал)"
+    },
+    "creationTime": {
+        "be": "1497 г.",
+        "ru": "1497 г."
+    },
+    "author": {
+        "be": "Іван III Васільевіч",
+        "ru": "Иван III Васильевич"
+    },
+    "languageId": oldEastSlavic._id,
+    "categoryId": docsCategory._id,
+    "subCategoryId": db.categories.findOne({ "name.be": "Права", "parentId": docsCategory._id })._id,
+    "fileId": db.fs.files.findOne({ filename: /Судебник_Ивана_1497_г\._оригинал\.pdf$/i })._id,
+    "tags": [
+        "TestTag0", "TestTag1"
+    ]
+})
+
+db.historicalDocs.insertOne({
+    "title": {
+        "be": "Судзебнік Івана 1497 г. (новая рэдакцыя)",
+        "ru": "Судебник Ивана 1497 г. (новая редакция)"
+    },
+    "creationTime": {
+        "be": "1497 г.",
+        "ru": "1497 г."
+    },
+    "author": {
+        "be": "Іван III Васільевіч",
+        "ru": "Иван III Васильевич"
+    },
+    "languageId": russian._id,
+    "categoryId": docsCategory._id,
+    "subCategoryId": db.categories.findOne({ "name.be": "Права", "parentId": docsCategory._id })._id,
+    "fileId": db.fs.files.findOne({ filename: /Судебник_Ивана_1497_г\._новая_редакция\.pdf$/i })._id,
     "tags": [
         "TestTag0", "TestTag1"
     ]
