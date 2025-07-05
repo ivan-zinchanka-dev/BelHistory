6b7771df1b007eb35112ed33f1980df91aeb1a34
Ivan Zinchanka <75093640+ivan-zinchanka-dev@users.noreply.github.com>
2025-07-05 23:13:57 +0300

    add: data loader project

---
+++ BelHistory.DataLoader/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.CommandLine;
+using System.CommandLine.Invocation;
+
+namespace BelHistory.DataLoader;
+
+public static class Program
+{
+    public static async Task<int> Main(string[] args)
+    {
+        RootCommand rootCommand = new RootCommand("Утилита для добавления данных");
+
+        var loadFilesCommand = new Command("files", "Загружает файлы в БД");
+        var directoryArg = new Argument<string>("dir");
+        
+        
+        loadFilesCommand.Add(directoryArg);
+        loadFilesCommand.SetHandler( directoryPath =>
+        {
+            Console.WriteLine("Path: " + directoryPath);
+            
+        }, directoryArg);
+        
+        
+        rootCommand.Add(loadFilesCommand);
+        
+        return await rootCommand.InvokeAsync(args);
+    }
+}
